3261377984c0f9748fd534eeaa1221578fe35900
Romeo Disca <romeo.disca@gmail.com>
2022-12-03 10:27:59 +0100

    async fibonnaci creates error

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,27 @@ fn fibo(n: u64) -> u64 {
     }
 }
 
+async fn async_fibo(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => async_fibo(n - 1).await + async_fibo(n - 2).await,
+    }
+}
+/*
+error[E0733]: recursion in an `async fn` requires boxing
+  --> src/main.rs:10:32
+   |
+10 | async fn async_fibo(n: u64) -> u64 {
+   |                                ^^^ recursive `async fn`
+   |
+   = note: a recursive `async fn` must be rewritten to return a boxed `dyn Future`
+   = note: consider using the `async_recursion` crate: https://crates.io/crates/async_recursion
+
+For more information about this error, try `rustc --explain E0733`.
+error: could not compile `fibonacci` due to previous error
+ */
+
 fn calculate_sync(n: u64) {
     println!("The fibonacci number of n = {} is {}.", n, fibo(n));
 }
