69e1b75df105fab2d836577051ba0eedab5f44cf
Romeo Disca <romeo.disca@gmail.com>
2022-12-03 10:24:07 +0100

    added fibonacci calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+
+fn fibo(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibo(n - 1) + fibo(n - 2),
+    }
+}
+
+fn calculate_sync(n: u64) {
+    println!("The fibonacci number of n = {} is {}.", n, fibo(n));
+}
+
 fn main() {
-    println!("Hello, world!");
+    calculate_sync(10);
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn sync_test() {
+        assert_eq!(fibo(0), 0);
+        assert_eq!(fibo(1), 1);
+        assert_eq!(fibo(2), 1);
+        assert_eq!(fibo(3), 2);
+        assert_eq!(fibo(4), 3);
+        assert_eq!(fibo(5), 5);
+        assert_eq!(fibo(10), 55);
+    }
 }
